e4d15b4ff8a8c6d5ebbf4a870ff000b0611f49b6
sathish palvai <sathish.palvai@us.abb.com>
2020-08-19 14:44:57 -0700

    added routing module and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-    <div className="App">
-       <HomePage/>
-    </div>
+  
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
+      </div>
+
   );
 }
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 export default App;
